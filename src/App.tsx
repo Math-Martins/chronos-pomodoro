2c99b43295773820797300cd1a38718866d6a1cf
Math-Martins <mhantunes.martins@gmail.com>
2025-07-22 17:46:31 -0300

    heading component finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { TimerIcon } from 'lucide-react'
 import { Heading } from './components/Heading'
 import './styles/global.css'
 import './styles/theme.css'
@@ -5,7 +6,21 @@ import './styles/theme.css'
 export function App() {
     return (
         <>
-            <Heading attr={123} attr2='String'>Hello World 1</Heading>
+            <Heading>
+                Hello World 1
+                <button>
+                    <TimerIcon />
+                </button>
+                <button>
+                    <TimerIcon />
+                </button>
+                <button>
+                    <TimerIcon />
+                </button>
+                <button>
+                    <TimerIcon />
+                </button>
+            </Heading>
             <p>
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reprehenderit optio quasi quisquam porro ut accusamus deleniti alias, perspiciatis esse accusantium itaque eaque tempora quia voluptate laborum minima error veniam fugiat?
             </p>
