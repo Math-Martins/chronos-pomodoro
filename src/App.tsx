67306ed6858c4ea2117d31d8b6310c54481cb4de
Math-Martins <mhantunes.martins@gmail.com>
2025-07-27 22:27:35 -0300

    components and icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { Container } from './components/Container'
-import { Heading } from './components/Heading'
+import { Logo } from './components/Logo'
+import { Menu } from './components/Menu'
+
 import './styles/global.css'
 import './styles/theme.css'
 
@@ -7,12 +9,12 @@ export function App() {
     return (
         <>
             <Container>
-                <Heading>LOGO</Heading>
+                <Logo />
             </Container>
             <Container>
-                <Heading>MENU</Heading>
+                <Menu />
             </Container>
-            
+
         </>
     )
 }
