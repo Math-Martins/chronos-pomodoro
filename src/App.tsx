8a17e337d49fbb5f965b44d38dab5e392b4bafd7
Math-Martins <mhantunes.martins@gmail.com>
2025-07-31 17:31:08 -0300

    component GenericHtml created

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import { AboutPomodoro } from './pages/AboutPomodoro'
 import { Home } from './pages/Home'
+import { NotFound } from './pages/NotFound'
 
 import './styles/global.css'
 import './styles/theme.css'
 export function App() {
-    return <Home />
+    return <AboutPomodoro />
 }
