c84e485aea4884b6433ce26606696f8b16416426
Math-Martins <mhantunes.martins@gmail.com>
2025-08-12 11:18:35 -0300

    global state with useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
-import { AboutPomodoro } from './pages/AboutPomodoro'
+import { useState } from 'react'
 import { Home } from './pages/Home'
-import { NotFound } from './pages/NotFound'
 
 import './styles/global.css'
 import './styles/theme.css'
+import type { TaskStateModel } from './models/TaskStateModel'
+
+const initalState: TaskStateModel = {
+    tasks: [],
+    secondsRemaining: 0,
+    formattedSecondsRemaining: '00:00',
+    activeTask: null,
+    currentCycle: 0,
+    config: {
+        workTime: 25,
+        shortBreakTime: 5,
+        longBreakTime: 15
+    }
+}
+
 export function App() {
-    return <Home />
+    const [state, setState] = useState(initalState)
+    
+    console.log('APP', state)
+    
+    return <Home state={state} setState={setState} />
 }
